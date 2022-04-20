e8e47e66a4f23e0fa539e9097afa17d1eae054aa
DESKTOP-TMUCHT9\franc <fjbfominaya@gmail.com>
2022-04-20 20:10:54 +0200

    test list_juegos

--- src/test/java/services/JuegoServiceTest.java
+++ src/test/java/services/JuegoServiceTest.java
@@ -2,13 +2,27 @@ package test.java.services;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import main.java.interfaces.IJuegoService;
+import main.java.modelo.Juego;
+import main.java.services.JuegoService;
 
 public class JuegoServiceTest {
 
+	IJuegoService juegoservice = new JuegoService();
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testistar_todos_juegos() {
+		ArrayList<Juego> listajuegos = new ArrayList<Juego>();
+		assertNotNull("Ver que no hay valores NULL", listajuegos);
 	}
 
 }
