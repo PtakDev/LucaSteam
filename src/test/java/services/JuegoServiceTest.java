bb29296682945cd745c5beb2aceaa87f1bfb2e15
PtakDev <79053918+PtakDev@users.noreply.github.com>
2022-04-20 21:53:23 +0200

    test_crear_nuevo_juego_vacio(), test_nombre_juego(), test_nombre_juego_null()

--- src/test/java/services/JuegoServiceTest.java
+++ src/test/java/services/JuegoServiceTest.java
@@ -1,9 +1,13 @@
 package test.java.services;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import main.java.interfaces.IJuegoService;
 import main.java.modelo.Juego;
@@ -13,7 +17,7 @@ public class JuegoServiceTest {
 
 	IJuegoService juegoservice = new JuegoService();
 	ArrayList<Juego> listajuegos = new ArrayList<Juego>();
-
+	Juego juego = new Juego();
 	
 	@Test
 	public void testlistar_todos_juegos() {
@@ -21,6 +25,23 @@ public class JuegoServiceTest {
 			Assert.assertNotNull("No hay valor NULL",j);
 		}
 
+	}
+	
+	@Test
+	public void test_crear_nuevo_juego_vacio() {
+		Assert.assertNotEquals(juego, null);
+	}
+	
+	@Test
+	public void test_nombre_juego() {
+		juego.setNombre("Antonio");
+		Assert.assertEquals(juego.getNombre(), "Antonio");
+	}
+	
+	@Test
+	public void test_nombre_juego_null() {
+		juego.setNombre(null);
+		Assert.assertEquals(juego.getNombre(), null);
 	}
 	
 
