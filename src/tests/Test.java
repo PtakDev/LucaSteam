b793b5ac2e8c15d7f89204b0cc00c714ecf82733
DESKTOP-TMUCHT9\franc <fjbfominaya@gmail.com>
2022-04-20 16:56:44 +0200

    prueba

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -21,7 +21,7 @@ public class Test {
 			BufferedReader br = new BufferedReader(new FileReader(".\\datos\\vgsales.csv"));
 			while ((line = br.readLine()) != null) // returns a Boolean value
 			{
-				String[] juego = line.split(splitBy); // use comma as separator
+				//String[] juego = line.split(splitBy); // use comma as separator
 				Juego videojuego = new Juego(Integer.parseInt(juego[0]), juego[1], juego[2], Integer.parseInt(juego[3]),
 						juego[4], juego[5]);
 				listajuegos.add(videojuego);
