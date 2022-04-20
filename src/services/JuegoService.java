b9f8a012630c7637066f47a951bcc637f78c4f19
franc <fjbfominaya@gmail.com>
2022-04-20 12:14:29 +0200

    prueba de commit

--- src/services/JuegoService.java
+++ src/services/JuegoService.java
@@ -17,15 +17,10 @@ public class JuegoService implements IJuegoService {
 	}
 
 	@Override
-	public void listar_juegos_genero_plataforma() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public void listar_todos_juegos() {
 		// TODO Auto-generated method stub
-
+//prueba de cambios
+		//prueba
 	}
 
 	@Override
